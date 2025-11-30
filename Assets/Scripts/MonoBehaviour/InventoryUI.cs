1b29adf2185c1b41a6929ee198b70dd4286be8e7
arthalm <arthurbr912@gmail.com>
2025-11-29 22:38:31 -0300

    finalização do playerinventory + hud

--- Assets/Scripts/MonoBehaviour/InventoryUI.cs
+++ Assets/Scripts/MonoBehaviour/InventoryUI.cs
@@ -5,30 +5,34 @@ public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private PlayerInventory playerInventory;
     [SerializeField] private Image inventoryImage;
-    [SerializeField] private Text inventoryText;
+    [SerializeField] private GameObject hudObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        inventoryImage.enabled = false;
+        HideHUD();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerInventory.CurrentItem != null)
+        if (playerInventory.CurrentItem != null)
         {
-            inventoryImage.enabled = true;
+            ShowHUD();
             inventoryImage.sprite = playerInventory.CurrentItem.Icon;
         }
         else
         {
-            inventoryImage.enabled = false;
+            HideHUD();
         }
     }
 
-    public void SetCurrentItem(Sprite icon, string itemName)
+    public void ShowHUD()
     {
-        inventoryImage.sprite = icon;
-        inventoryText.text = "Item no inventário " + itemName;
+        hudObject.SetActive(true);
+    }
+
+    public void HideHUD()
+    {
+        hudObject.SetActive(false);
     }
 }
